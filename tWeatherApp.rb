9afa880879345ca50e6b3926b70f99689faa06a6
Elijah Gutman <gutmane7@gmail.com>
2024-10-31 15:54:48 -0400

    weather

---
+++ tWeatherApp.rb
@@ -0,0 +1,39 @@
+require "http"
+list = ["chicago", "new york", "san fransisco", "los angeles", "boston"]
+puts "Welcome to the weather app!"
+while true
+  print "What city are you in? (enter q to quit program): "
+  city = gets.chomp.downcase
+  if city == "q"
+    system "clear"
+    puts "See you later!"
+    return
+  elsif !list.include?(city)
+    puts "Type in a valid city from \"chicago\", \"new york\", \"san fransisco\", \"los angeles\", \"boston\""
+  else
+    response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&units=imperial&appid=#{ENV["OPEN_WEATHER_API_KEY"]}")
+    data = response.parse
+    temp = data["main"]["temp"]
+    description = data["weather"][0]["description"]
+    puts "The temperature in #{city} is #{temp} with #{description}"
+    lat = data["coord"]["lat"]
+    lon = data["coord"]["lon"]
+    response2 = HTTP.get("https://api.openweathermap.org/data/2.5/forecast?lat=#{lat}&lon=#{lon}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=imperial")
+    data2 = response2.parse
+    print "The 3 day forecast is: "
+    n = 1
+    3.times do
+      temp_future = data2["list"][8 * n]["main"]["temp"]
+      print "#{temp_future} "
+      n += 1
+    end
+    puts
+    if description == "rain"
+      puts ":rain_cloud::rain_cloud::rain_cloud:"
+    elsif description == "sunny"
+      puts ":sun_with_face::sun_with_face::sun_with_face:"
+    elsif description == "clear sky"
+      puts ":cityscape::cityscape::cityscape:"
+    end
+  end
+end
